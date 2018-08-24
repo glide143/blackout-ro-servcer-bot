f270c8b9d3d5f885b176a37cfdef9fed281a9430
Melchizedek Aguinaldo <melchizedek30@gmail.com>
2018-08-24 18:58:38 +0800

    added simple example
    Signed-off-by: Melchizedek Aguinaldo <melchizedek30@gmail.com>

--- index.js
+++ index.js
@@ -12,21 +12,7 @@ const { dialogflow } = require('actions-on-google');
   // Register handlers for Dialogflow intents
    
   app.intent('Default Welcome Intent', conv => {
-    conv.ask('Hi, how is it going?')
-    conv.ask(`Here's a picture of a cat`)
-    conv.ask(new Image({
-      url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-      alt: 'A cat',
-    }))
-  });
-   
-  // Intent in Dialogflow called `Goodbye`
-  app.intent('Goodbye', conv => {
-    conv.close('See you later!')
-  });
-   
-  app.intent('Default Fallback Intent', conv => {
-    conv.ask(`I didn't understand. Can you tell me something else?`)
+    conv.ask('How are you?');
   });
 
   const expressApp = express().use(bodyParser.json());
